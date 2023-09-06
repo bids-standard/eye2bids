e980eeb949cda4943959f8cd6ed49dae8232815b
Remi Gau <remi_gau@hotmail.com>
2023-09-06 11:51:38 -0400

    reset and drop data

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # eye2bids
+
+## Instructions for testing conversion_json
+
+### Requirements
+
+- edf file by EyeLink Eye Tracker
+- Python3
+- EyeLink Developers Kit. Download from [SR-Research support forum] (forum registration required)
+- manual_metadata.yml file (find template and an example in conversion_json folder)
+
+### Run code
+```
+python3 edf2bids_json.py
+```
+
+[SR-Research support forum]: https://www.sr-research.com/support/forum-9.html
---
+++ tools/download_test_data.py
@@ -0,0 +1,34 @@
+"""Download test data from OSF, unzip and install in the correct location."""
+
+import os
+import zipfile
+import shutil
+import requests
+
+from pathlib import Path
+
+download_url = "https://files.de-1.osf.io/v1/resources/jdv7n/providers/osfstorage/?zip="
+
+root_dir = Path(__file__).parent.parent
+
+output_dir = root_dir / "tests" / "data"
+
+if output_dir.exists():
+    shutil.rmtree(output_dir)
+output_dir.mkdir(parents=True)
+
+output_file = output_dir / "test_data.zip"
+
+# Download the zip file
+r = requests.get(download_url, stream=True)
+with open(output_file, "wb") as f:
+    for chunk in r.iter_content(chunk_size=1024):
+        if chunk:
+            f.write(chunk)
+
+# unzip the file
+with zipfile.ZipFile(output_file, "r") as zip_ref:
+    zip_ref.extractall(output_dir / ".")
+    os.remove(output_file)
+
+
