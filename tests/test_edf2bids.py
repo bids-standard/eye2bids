59a6d35cdd55c25e73ce9e0d6d2540ba18643d48
Remi Gau <remi_gau@hotmail.com>
2023-09-07 11:05:01 -0400

    add more tests

--- tests/test_edf2bids.py
+++ tests/test_edf2bids.py
@@ -9,7 +9,10 @@ from eye2bids.edf2bids import (
     _extract_CalibrationPosition,
     _extract_CalibrationType,
     _extract_CalibrationUnit,
+    _extract_EyeTrackingMethod,
+    _extract_SamplingFrequency,
     _extract_ScreenResolution,
+    _load_asc_file,
     _load_asc_file_as_reduced_df,
     edf2bids,
 )
@@ -72,7 +75,7 @@ def test_edf_end_to_end(metadata_file):
     [
         ("decisions", "HV9"),
         ("emg", "HV9"),
-        ("lt", "FIXME"),
+        ("lt", "HV9"),
         ("pitracker", "HV9"),
         ("rest", "HV13"),
         ("satf", "HV9"),
@@ -91,7 +94,7 @@ def test_extract_CalibrationType(folder, expected):
     [
         ("decisions", [1919, 1079]),
         ("emg", [1919, 1079]),
-        ("lt", "FIXME"),
+        ("lt", [1919, 1079]),
         ("pitracker", [1919, 1079]),
         ("rest", "FIXME"),
         ("satf", [1919, 1079]),
@@ -110,7 +113,7 @@ def test_extract_ScreenResolution(folder, expected):
     [
         ("decisions", ""),
         ("emg", ""),
-        ("lt", "FIXME"),
+        ("lt", "pixel"),
         ("pitracker", ""),
         ("rest", "pixel"),
         ("satf", ""),
@@ -129,7 +132,33 @@ def test_extract_CalibrationUnit(folder, expected):
     [
         ("decisions", []),
         ("emg", []),
-        ("lt", "FIXME"),
+        (
+            "lt",
+            [
+                [
+                    [960, 540],
+                    [960, 324],
+                    [960, 755],
+                    [576, 540],
+                    [1343, 540],
+                    [622, 350],
+                    [1297, 350],
+                    [622, 729],
+                    [1297, 729],
+                ],
+                [
+                    [960, 540],
+                    [960, 324],
+                    [960, 755],
+                    [576, 540],
+                    [1343, 540],
+                    [622, 350],
+                    [1297, 350],
+                    [622, 729],
+                    [1297, 729],
+                ],
+            ],
+        ),
         ("pitracker", []),
         (
             "rest",
@@ -160,3 +189,41 @@ def test_extract_CalibrationPosition(folder, expected):
     asc_file = asc_test_files(input_dir=input_dir)[0]
     df_ms_reduced = _load_asc_file_as_reduced_df(asc_file)
     assert _extract_CalibrationPosition(df_ms_reduced) == expected
+
+
+@pytest.mark.parametrize(
+    "folder, expected",
+    [
+        ("decisions", "P-CR"),
+        ("emg", "P-CR"),
+        ("lt", "P-CR"),
+        ("pitracker", "P-CR"),
+        ("rest", "P-CR"),
+        ("satf", "P-CR"),
+        ("vergence", "P-CR"),
+    ],
+)
+def test_extract_EyeTrackingMethod(folder, expected):
+    input_dir = data_dir() / "osf" / "eyelink" / folder
+    asc_file = asc_test_files(input_dir=input_dir)[0]
+    events = _load_asc_file(asc_file)
+    assert _extract_EyeTrackingMethod(events) == expected
+
+
+@pytest.mark.parametrize(
+    "folder, expected",
+    [
+        ("decisions", 1000),
+        ("emg", 1000),
+        ("lt", 1000),
+        ("pitracker", 1000),
+        ("rest", 1000),
+        ("satf", 500),
+        ("vergence", 1000),
+    ],
+)
+def test_extract_SamplingFrequency(folder, expected):
+    input_dir = data_dir() / "osf" / "eyelink" / folder
+    asc_file = asc_test_files(input_dir=input_dir)[0]
+    df_ms_reduced = _load_asc_file_as_reduced_df(asc_file)
+    assert _extract_SamplingFrequency(df_ms_reduced) == expected
