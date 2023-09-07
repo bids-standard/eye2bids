e65ccc4448dc64f8ca9638f62370e8fe889949f2
Remi Gau <remi_gau@hotmail.com>
2023-09-06 22:13:37 -0400

    add pyaml

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -2,8 +2,11 @@ from pathlib import Path
 
 from eye2bids.edf2bids import main
 
+# import pytest
 
-def test_end_to_end():
+
+# @pytest.mark.skip(reason="Not implemented yet")
+def test_edf_end_to_end():
     data_dir = Path(__file__).parent / "data"
     input_file = data_dir / "decisions_modality_baptisteC2_7-5-2016_21-26-13.edf"
     metadata_file = data_dir / "metadata.yml"
