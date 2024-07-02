cc167f7458771692ae7792fbcf02fb6c109c2f9a
julia-pfarr <juliakatharina.pfarr@gmx.de>
2024-07-02 16:03:40 +0200

    change order of samples in order to have timestamp appear first

--- eye2bids/edf2bids.py
+++ eye2bids/edf2bids.py
@@ -548,7 +548,7 @@ def edf2bids(
 
     samples = pd.read_csv(samples_asc_file, sep="\t", header=None)
     samples_eye1 = (
-        pd.DataFrame(samples.iloc[:, [2, 1, 3, 0]])
+        pd.DataFrame(samples.iloc[:, [0, 1, 2, 3]])
         .map(lambda x: x.strip() if isinstance(x, str) else x)
         .replace(".", np.nan, regex=False)
     )
