08e8c6985e1c35fc9faea891b5f2189e44f9276c
Julia-Katharina Pfarr <111446107+julia-pfarr@users.noreply.github.com>
2024-07-08 19:20:35 +0200

    Fix bug: _extract_EyeTrackingMethod (#82)
    * Tried to fix bugs, but failed
    
    edf2asc suddenly gives out a random order for the calibration positions. E.g., calibration point 3 is listed after calibration point 4. That's why I had to change the function _extract_Calibration poisition (as well as _get_calibration_positions). However, now the tests are failing because something is wrong with the lists in lists thingi and also it tries to get the calibration positions also for the testfiles which have none. I thought I specified that within the functions but obviously it doesn't work.
    
    * fix bug: make sure that EyeTrackingMethod is empty if no calibration because otherwise it writes wrong content into the variable
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix test: parantheses
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add PhysioType to metadata
    
    * Update _base.py
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- eye2bids/_base.py
+++ eye2bids/_base.py
@@ -78,10 +78,12 @@ class BasePhysioJson(dict[str, Any]):
     input_file: Path
     has_validation: bool
     two_eyes: bool
+    has_calibration: bool
 
     def __init__(self, manufacturer: str, metadata: dict[str, Any] | None = None) -> None:
 
         self["Manufacturer"] = manufacturer
+        self["PhysioType"] = "eyetrack"
 
         self["Columns"] = ["x_coordinate", "y_coordinate", "pupil_size", "timestamp"]
         self["timestamp"] = {
