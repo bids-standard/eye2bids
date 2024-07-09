ef59c1c3a6882f466137d41c07c23798e1a61465
Remi Gau <remi_gau@hotmail.com>
2024-07-09 11:56:34 +0200

    Apply suggestions from code review

--- eye2bids/edf2bids.py
+++ eye2bids/edf2bids.py
@@ -46,25 +46,26 @@ def _check_inputs(
         raise FileNotFoundError(f"No such input file: {cheked_input_file}")
 
     if metadata_file in [None, ""]:
-        e2b_log.info(
+        e2b_log.warning(
             """Load the metadata.yml file with the additional metadata.\n
             You can find a template in the eye2bids GitHub.\n
             This file must contain at least the additional REQUIRED metadata\n
             in the format specified in the BIDS specification.\n"""
         )
-        metadata_file = Prompt.ask("Enter the file path to the metadata.yml file")
 
-        if metadata_file in ["", None] and not force:
-            e2b_log.info(
+        if interactive:
+          metadata_file = Prompt.ask("Enter the file path to the metadata.yml file")
+          if metadata_file in ["", None]:
+            if not force:
+              e2b_log.error(
                 """You didn't pass a metadata.yml file. As this file contains metadata\n
                 which is REQUIRED for a valid BIDS dataset, the conversion process now\n
                 stops. Please start again with a metadata.yml file\n
                 or run eye2bids in force mode.\n
                 (will produce an invalid BIDS dataset).\n"""
             )
-            raise SystemExit(1)
+              raise SystemExit(1)
 
-        elif metadata_file in ["", None] and force:
             checked_metadata_file = None
 
     elif isinstance(metadata_file, str):
