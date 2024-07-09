cd3d9ea1408aaa18e5922486c86d1bab6bc349c8
Remi Gau <remi_gau@hotmail.com>
2024-07-09 13:29:03 +0200

    fix tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -18,7 +18,7 @@ def root_dir() -> Path:
 @pytest.mark.skipif(not _check_edf2asc_present(), reason="edf2asc missing")
 @pytest.mark.parametrize("output_dir", [data_dir() / "output", None])
 @pytest.mark.parametrize("use_relative_path", [False, True])
-def test_edf_cli(use_relative_path, metadata_file, output_dir, eyelink_test_data_dir):
+def test_edf_cli(use_relative_path, output_dir, eyelink_test_data_dir):
 
     metadata_file = data_dir() / "metadata.yml"
 
@@ -27,14 +27,14 @@ def test_edf_cli(use_relative_path, metadata_file, output_dir, eyelink_test_data
 
     if use_relative_path:
         input_file = input_file.relative_to(root_dir())
+        metadata_file = metadata_file.relative_to(root_dir())
 
-    command = ["eye2bids", "--input_file", str(input_file)]
-    if metadata_file is not None:
-        if use_relative_path:
-            metadata_file = metadata_file.relative_to(root_dir())
-        metadata_file = str(metadata_file)
-        command.extend(["--metadata_file", metadata_file])
-
+    command = [
+        "eye2bids",
+        "--input_file",
+        str(input_file),
+        *["--metadata_file", str(metadata_file)],
+    ]
     if output_dir is not None:
         if use_relative_path:
             output_dir = output_dir.relative_to(root_dir())
