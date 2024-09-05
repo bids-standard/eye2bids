5fc375f4b33a76109a090a64223954af290df862
Remi Gau <remi_gau@hotmail.com>
2024-09-05 15:33:39 +0200

    mark test as xfail

--- tests/test_edf2bids.py
+++ tests/test_edf2bids.py
@@ -82,7 +82,6 @@ def _check_output_content(output_dir, input_file, eye=1):
 @pytest.mark.parametrize(
     "folder",
     [
-        "emg",
         "lt",
         "pitracker",
         "rest",
@@ -91,6 +90,30 @@ def _check_output_content(output_dir, input_file, eye=1):
 )
 @pytest.mark.skipif(not _check_edf2asc_present(), reason="edf2asc missing")
 def test_edf_end_to_end_all(eyelink_test_data_dir, folder):
+    """Run conversion of all test datasets and check output."""
+    metadata_file = data_dir() / "metadata.yml"
+
+    input_dir = eyelink_test_data_dir / folder
+    input_file = edf_test_files(input_dir=input_dir)[0]
+
+    output_dir = data_dir() / "output"
+    output_dir.mkdir(exist_ok=True)
+
+    edf2bids(input_file=input_file, metadata_file=metadata_file, output_dir=output_dir)
+
+    _check_output_exists(output_dir, input_file)
+    _check_output_content(output_dir, input_file)
+
+
+@pytest.mark.xfail(reason="Output is not a continuous recording. See #69.")
+@pytest.mark.parametrize(
+    "folder",
+    [
+        "emg",
+    ],
+)
+@pytest.mark.skipif(not _check_edf2asc_present(), reason="edf2asc missing")
+def test_edf_end_to_end_emg_xfail(eyelink_test_data_dir, folder):
     """Run conversion of all test datasets and check output."""
     metadata_file = data_dir() / "metadata.yml"
 
